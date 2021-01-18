4bfcc20c2c015b2418d087ecc49497f2a6f4926e
Ilisha29 <94konzern@gmail.com>
2021-01-18 23:57:39 +0900

    add : Project_A

---
+++ Project_A/ddl.sql
@@ -0,0 +1,17 @@
+CREATE TABLE businessCard(
+	name VARCHAR(20),
+	phone VARCHAR(13),
+	companyName VARCHAR(20),
+	createDate TIMESTAMP,
+);
+
+INSERT INTO businessCard VALUES ("홍길동","010-6609-1032", "NAVER");
+INSERT INTO businessCard VALUES ("김민섭","010-1234-5435", "KAKAO");
+INSERT INTO businessCard VALUES ("길동홍","010-7843-1279", "LINE");
+INSERT INTO businessCard VALUES ("김홍민","010-5462-7531", "COUPANG");
+INSERT INTO businessCard VALUES ("김연주","010-1175-0086", "BAEMIN");
+INSERT INTO businessCard VALUES ("심기성","010-1962-2397", "NEXON");
+INSERT INTO businessCard VALUES ("최주환","010-1249-1234", "11ST");
+INSERT INTO businessCard VALUES ("박준수","010-0864-7232", "NC");
+INSERT INTO businessCard VALUES ("이혁진","010-1973-8252", "SAMSUNG");
+INSERT INTO businessCard VALUES ("정병재","010-2135-0983", "SK");
